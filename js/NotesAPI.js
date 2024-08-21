dfe6d3f9a86ab18d07796a351cd9e5fd3f72a9b3
randomsapiens1 <imsapiens.ctf@gmail.com>
2024-08-22 01:00:01 +0600

    notes

---
+++ js/NotesAPI.js
@@ -0,0 +1,14 @@
+export default class NotesAPI {
+    static getAllNotes(){
+
+    }
+
+    //not only save note, but also to update new note
+    static saveNote(noteToSave){
+
+    }
+
+    static deleteNote(noteToDelete){
+
+    }
+}
